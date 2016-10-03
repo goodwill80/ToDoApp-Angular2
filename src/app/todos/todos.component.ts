b5924fef3e15ffda63c93290be8f9046fcc36bd8
kit jonathan ho <j.hokit80@gmail.com>
2016-10-03 19:22:10 +0800

    revision todos Anguar

---
+++ src/app/todos/todos.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+
+
+class Todo {
+  text: string;
+  isCompleted: boolean;
+
+  constructor(text: string, isCompleted: boolean){
+    this.text = text;
+    this.isCompleted = isCompleted;
+  }
+}
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+
+export class TodosComponent {
+  todos: Todo[];
+  constructor(){
+    this.todos = [];
+  }
+
+  addTodo(newtodo: HTMLInputElement, isCompleted) {
+
+    var newTask = newtodo.value;
+    this.todos.push(new Todo(newTask));
+    newtodo.value = "";
+
+  }
+
+
+  removeTodo(todo) : void {
+     var index = this.todos.indexOf(todo, 0);
+     if (index !== undefined) {
+       this.todos.splice(index, 1);
+     }
+  }
+
+  get remaining() {
+      return this.todos.reduce((count: number, todo: Todo) => count + +!todo.isCompleted, 0);
+    }
+
+    archive(): void {
+      var oldTodos = this.todos;
+      this.todos = [];
+      oldTodos.forEach((todo: Todo) => {
+        if (!todo.isCompleted) this.todos.push(todo);
+      });
+    }
+
+}
